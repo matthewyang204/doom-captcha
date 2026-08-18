9e8c461f2b5eba79270eac11de7484c7f778fbe4
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-08-18 13:46:04 -0700

    Allow all these module properties

--- build.sh
+++ build.sh
@@ -30,8 +30,6 @@ cd sdldoom-1.10 || exit
 export EMCC_CFLAGS="-std=gnu89 -sUSE_SDL"
 emconfigure ./configure
 emmake make
-emcc -o index.html ./*.o --preload-file doom1.wad \
-	-s ALLOW_MEMORY_GROWTH=1 -s 'INCOMING_MODULE_JS_API=["keyboardListeningElement"]' --shell-file ../shell.html
-
+emcc -o index.html ./*.o --preload-file doomu.wad -s ALLOW_MEMORY_GROWTH=1 -s 'INCOMING_MODULE_JS_API=["keyboardListeningElement","arguments","preRun","postRun","print","printErr","canvas","setStatus","monitorRunDependencies"]' --shell-file ../shell.html
 mkdir -p ../public
 mv index.* ../public
